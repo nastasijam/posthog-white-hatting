6d7ebcb91d9055e5b6016b21b8a2a765c6525106
Haven <haven@posthog.com>
2025-11-17 18:09:43 +0100

    tweak

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5149,6 +5149,7 @@ export type BatchExportServiceDatabricks = {
 
 export type BatchExportRealtimeDestinationBackfill = {
     type: 'PostHogRealtimeDestinations'
+    config: {}
 }
 
 // When adding a new option here also add a icon for it to

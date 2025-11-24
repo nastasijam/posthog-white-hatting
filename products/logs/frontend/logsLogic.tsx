8ec210df1ab07bf99f29062b5984632161356796
Jon McCallum <jonathon@posthog.com>
2025-11-24 10:35:09 +0000

    refactor(logs): only fetch logs on loadmorelogs

--- products/logs/frontend/logsLogic.tsx
+++ products/logs/frontend/logsLogic.tsx
@@ -413,7 +413,7 @@ export const logsLogic = kea<logsLogicType>([
             actions.fetchSparkline()
         },
         loadMoreLogs: () => {
-            actions.runQuery()
+            actions.fetchLogs()
         },
         cancelInProgressLogs: ({ logsAbortController }) => {
             if (values.logsAbortController !== null) {
